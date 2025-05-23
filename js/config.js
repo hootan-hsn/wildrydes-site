fdc729b71c2d196bca00aaadc4c01b15a89f413e
hootan-hsn <BooM11211@outlook.com>
2025-05-22 20:56:59 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_vscS0Wi08', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7ece2rtc80f2qu32ul1ut1a6rk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        client_secret: '1acfgcs1juvc7rclplvk5bjuf7s5lcfr187vf54ialim78djo095',
+        userPoolId: 'us-east-1_7sVBM979Q', // e.g. us-east-2_uXboG5pAb
+        client_id: '73ns5j0am9dreq4iarhvjbf9pu',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
