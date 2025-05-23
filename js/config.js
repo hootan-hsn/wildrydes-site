09fd64ab14ad2b6d13aeff26c6773d456d3411dc
hootan-hsn <BooM11211@outlook.com>
2025-05-22 21:30:49 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_1erWJ4syX', // e.g. us-east-2_uXboG5pAb
-        client_id: '2vieuoe501po1fvbkjfc98a5q1',
+        userPoolClientId: '2vieuoe501po1fvbkjfc98a5q1',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
